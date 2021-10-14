c74296fbafda28ee9b70c6098c96287f9413fc30
Yueyang Wu <yueyangwu983@gmail.com>
2021-10-13 23:38:26 -0700

    add environment variable AWS_ENV, clean up code

--- basic_ecommerce/settings.py
+++ basic_ecommerce/settings.py
@@ -10,14 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+
+# AWS ENV
+AWS_ENV = os.getenv("AWS_ENV")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-e^&l%88l3h03u!&hwv-2iy&ec^t@z%(p79#%*l06(1-slyz6f2'
@@ -40,14 +41,14 @@ INSTALLED_APPS = [
     'django_s3_storage',
 ]
 
-S3_BUCKET_NAME = "basic-ecommerce-static-files"
-STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
-AWS_S3_BUCKET_NAME_STATIC = S3_BUCKET_NAME
-# serve the static files directly from the specified s3 bucket
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % S3_BUCKET_NAME
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-# if you have configured a custom domain for your static files use:
-#AWS_S3_PUBLIC_URL_STATIC = "https://static.yourdomain.com/"
+STATIC_URL = '/static/'
+if AWS_ENV == 'true':
+    S3_BUCKET_NAME = "basic-ecommerce-static-files"
+    STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
+    AWS_S3_BUCKET_NAME_STATIC = S3_BUCKET_NAME
+    # serve the static files directly from the specified s3 bucket
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % S3_BUCKET_NAME
+    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,14 +123,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-#STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
